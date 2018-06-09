6acaa1991c0d9668615e695edd39bbd50c552b77
Vigneshraj Sekar Babu <vigneshrajsekarbabu@vigneshrajs-macbook-pro.local>
2018-06-09 13:30:19 -0500

    Initial Commit

---
+++ Visual Recognizer with rating/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Visual Recognizer with rating
+//
+//  Created by Vigneshraj Sekar Babu on 6/9/18.
+//  Copyright © 2018 Vigneshraj Sekar Babu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
